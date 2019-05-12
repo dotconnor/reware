179475c569c37e7e87514ed6a149a30140230280
Connor Love <connor@dotconnor.com>
2019-05-12 19:41:36 -0400

    feat: add s3 support (#12)
    * feat: add s3 support
    
    * add test
    
    * yeah
    
    * add aws stuff
    
    * expose secrets
    
    * ensure secret is always there
    
    * skip s3 tests

--- src/sources/s3.ts
+++ src/sources/s3.ts
@@ -1,7 +1,96 @@
-import { BaseSource } from "./base"
+import { S3, SharedIniFileCredentials } from "aws-sdk"
+// eslint-disable-next-line no-unused-vars
+import { BaseSource, SourceOptions, FileNotFoundError } from "./base"
+
+export interface S3SourceOptions extends SourceOptions {
+  bucket: string
+  region?: string
+  accessKeyId?: string
+  secretAccessKey?: string
+  profile?: string
+}
+
+const DEFAULT_OPTIONS = {
+  region: `us-east-1`,
+}
+
+function getS3Options(opts: S3SourceOptions): any {
+  let opt: any = {
+    region: opts.region,
+  }
+  if (opts.accessKeyId || opts.secretAccessKey) {
+    if (!(opts.accessKeyId && opts.secretAccessKey)) {
+      throw new Error(
+        `If either AWS Access Key or Secret Key is provided then both are required.`
+      )
+    }
+    opt = {
+      ...opt,
+      accessKeyId: opts.accessKeyId,
+      secretAccessKey: opts.secretAccessKey,
+    }
+  }
+  if (opts.profile) {
+    const cred = new SharedIniFileCredentials({ profile: opts.profile })
+    opt = {
+      ...opt,
+      credentials: cred,
+    }
+  }
+  return opt
+}
+
+function listObjectsAsync(
+  client: S3,
+  options: S3.ListObjectsV2Request
+): Promise<S3.ListObjectsV2Output> {
+  return new Promise((resolve, reject) => {
+    client.listObjectsV2(options, (err, data) => {
+      if (err) {
+        return reject(err)
+      }
+      return resolve(data)
+    })
+  })
+}
+
+function getObjectAsync(
+  client: S3,
+  options: S3.GetObjectRequest
+): Promise<S3.GetObjectOutput> {
+  return new Promise((resolve, reject) => {
+    client.getObject(options, (err, data) => {
+      if (err) {
+        return reject(err)
+      }
+      return resolve(data)
+    })
+  })
+}
 
 export class S3Source extends BaseSource {
-  get(key: string) {
-    return Promise.resolve(Buffer.alloc(0))
+  s3options: S3SourceOptions
+  client: S3
+  constructor(options: S3SourceOptions) {
+    super(options)
+    if (!options) {
+      throw new Error(`FileSystemSource requires a options parameter.`)
+    }
+    this.s3options = { ...DEFAULT_OPTIONS, ...options }
+    this.client = new S3(getS3Options(this.s3options))
+  }
+  async get(key: string, req?: Express.Request) {
+    const { Contents: matches } = await listObjectsAsync(this.client, {
+      Bucket: this.s3options.bucket,
+      Prefix: key,
+    })
+    if (!matches || matches.length === 0) {
+      throw new FileNotFoundError(`File not found on S3`, key)
+    }
+    const file = await getObjectAsync(this.client, {
+      Bucket: this.s3options.bucket,
+      Key: matches[0].Key!,
+    })
+    return file.Body as Buffer
   }
 }
---
+++ test/sources/s3.ts
@@ -0,0 +1,15 @@
+import test from "ava"
+import is from "@sindresorhus/is"
+import { S3Source } from "../../src/sources/s3"
+
+const testFunction = () => {
+  return typeof process.env.JENKINS_NODE_COOKIE === `undefined`
+    ? test
+    : test.skip
+}
+
+testFunction()(`Source get() with test`, async (t) => {
+  const source = new S3Source({ bucket: `reware-tests` })
+  const file = await source.get(`logo`)
+  t.true(is.buffer(file))
+})
