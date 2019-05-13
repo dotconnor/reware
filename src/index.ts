47650db1ef9f026013e4ebe0bef9782a57a94e66
Connor Love <connor@dotconnor.com>
2019-05-12 21:20:50 -0400

    fix: export all members (#15)
    * export all members
    
    * Update package.json

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import reware from "./reware"
-export default reware
+export { BaseSource, SourceOptions } from "./sources/base"
+export { FileSystemSource, FileSystemSourceOptions } from "./sources/fs"
+export { S3Source, S3SourceOptions } from "./sources/s3"
+
 module.exports = reware
+Object.assign(module.exports, exports)
